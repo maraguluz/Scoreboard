40d447931ce0e4a5f3f404568bcc0b827c4bdd0c
Mara Guluzian <mara15.guluzian@gmail.com>
2022-10-11 14:50:17 -0400

    disaster

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,22 @@
+// @ts-nocheck
 import './style.css'
 
-function updateTeam(id: string){
+function setupTeam(id: string){
   console.log(id)
+  let score = 0;
 const inputElement = document.querySelector(`.${id} input`) as HTMLInputElement
 
 const headers = document.querySelector(`.${id} h2`) as HTMLHeadingElement
+
+const addButton = document.querySelector(`.${id} .add`) as HTMLElement
+//the I tag is used as an icon and then use CSS to add an image to it 
+// is the same as const addButton = document.querySelector('.add) or (.team1 .add)
+
+const minusButton = document.querySelector(`.${id} .subtract`) as HTMLElement
+
+const scoreElement = document.querySelector(`.${id} .h3`) as HTMLHeadElement
+
+const resetButton = document.querySelector('.reset') as HTMLButtonElement
 
 //when the input happens i want you to set the header as that value 
 //which paragraph team1 or team2? 
@@ -18,12 +30,50 @@ console.log('Update',headers)
     const currentInputValue = elementChanged.value
     headers.innerText = currentInputValue
   }
-  }
+}
 inputElement?.addEventListener('input', updateName)
 // opporator that says if this is null dont do the thing on the right 
-}
-updateTeam('team1')
-updateTeam('team2')
+
+// function displayWinner(){
+//   alert('You win!');
+// }
+// //let score = 0;
+
+// function resetScore(event: Event){
+//   score = 0
+//   scoreElement.innerText = score.toString()
+// }
+// resetButton?.addEventListener('click', resetScore)
+
+// function incrementScore(event: Event){
+//   if (score < 22){
+//     score += 1
+//     scoreElement.innerText = score.toString()
+//   }  
+//   if (score ===21){
+//     displayWinner()
+//   }
+// }  
+// // addButton.addEventListener('click', ()=>(console.log('add'))) this is how you sanity check in your console on the browser 
+// addButton?.addEventListener('click', incrementScore)
+
+// function decreaseScore(event: Event){
+//   if (score > 0) {
+//     score -= 1
+//     scoreElement.innerText = score.toString()
+//   }  
+// }
+// minusButton?.addEventListener('click', decreaseScore)  
+
+
+
+// }
+
+
+
+
+setupTeam('team1')
+setupTeam('team2')
 //if you put anything in updateTeam thats what would console.log in line 4
 //but then on line 5 the queryselector wouldnt be able to find that class if it doesnt exist (it would be null)
 // so a closure is a snapshot or an occurance of what variables are in scope 
