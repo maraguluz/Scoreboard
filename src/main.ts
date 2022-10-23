3489d741dd6b04fa9689372551fce0eb59274ed3
maraguluz <mara15.guluzian@gmail.com>
2022-10-23 18:08:27 -0400

    Revert "test"
    This reverts commit 3910afd5cfaf3a8688fb789fc5648a5cc63c7220.

--- src/main.ts
+++ src/main.ts
@@ -3,19 +3,18 @@ import './style.css'
 
 function setupTeam(id: string){
   console.log(id)
-  let score = 0
+  let score = 0;
 const inputElement = document.querySelector(`.${id} input`) as HTMLInputElement
 
 const headers = document.querySelector(`.${id} h2`) as HTMLHeadingElement
 
-const addButton = document.querySelector('.add') as HTMLElement
-//const addButton = document.querySelector(`.${id} .add`) as HTMLElement
+const addButton = document.querySelector(`.${id} .add`) as HTMLElement
 //the I tag is used as an icon and then use CSS to add an image to it 
 // is the same as const addButton = document.querySelector('.add) or (.team1 .add)
 
 const minusButton = document.querySelector(`.${id} .subtract`) as HTMLElement
 
-const scoreElement = document.querySelector(`.${id} h3`)! as HTMLHeadElement
+const scoreElement = document.querySelector(`.${id} .h3`) as HTMLHeadElement
 
 const resetButton = document.querySelector('.reset') as HTMLButtonElement
 
@@ -23,62 +22,52 @@ const resetButton = document.querySelector('.reset') as HTMLButtonElement
 //which paragraph team1 or team2? 
 //IN JS when this function got created 
 console.log({inputElement, headers})
-
 function updateName(event: Event){
+  
   const elementChanged = event.target
-  console.log('Update',headers)
+console.log('Update',headers)
   if(elementChanged instanceof HTMLInputElement){
     const currentInputValue = elementChanged.value
     headers.innerText = currentInputValue
   }
 }
 inputElement?.addEventListener('input', updateName)
-
 // opporator that says if this is null dont do the thing on the right 
 
- function displayWinner(){
-   alert('You win!');
- }
+// function displayWinner(){
+//   alert('You win!');
+// }
 // //let score = 0;
 
+// function resetScore(event: Event){
+//   score = 0
+//   scoreElement.innerText = score.toString()
+// }
+// resetButton?.addEventListener('click', resetScore)
 
-function incrementScore(event: Event){
-  if (score<22){
-    score+=1
-    scoreElement.innerText = score.toString()
-  }
-  if (score ===21){
-    displayWinner()
-  }
-}
-addButton?.addEventListener('click', incrementScore)
+// function incrementScore(event: Event){
+//   if (score < 22){
+//     score += 1
+//     scoreElement.innerText = score.toString()
+//   }  
+//   if (score ===21){
+//     displayWinner()
+//   }
+// }  
 // // addButton.addEventListener('click', ()=>(console.log('add'))) this is how you sanity check in your console on the browser 
+// addButton?.addEventListener('click', incrementScore)
 
-
-function decreaseScore(event: Event){
-  if (score > 0) {
-    score -= 1
-    scoreElement.innerText = score.toString()
-  }  
-}
-minusButton?.addEventListener('click', decreaseScore)  
-
-function resetScore(event: Event){
-  score = 0
-  scoreElement.innerText = score.toString()
-}
-resetButton?.addEventListener('click', resetScore)
-}
+// function decreaseScore(event: Event){
+//   if (score > 0) {
+//     score -= 1
+//     scoreElement.innerText = score.toString()
+//   }  
+// }
+// minusButton?.addEventListener('click', decreaseScore)  
 
 
 
-
-
-
-
-
-
-
+// }
 
 
 
@@ -93,3 +82,4 @@ setupTeam('team2')
 
 //want to use .querySelector as little as possible because that will take a while 
 //thats why we cant put in const inputElement and const headers in the function below to make it one function
+
